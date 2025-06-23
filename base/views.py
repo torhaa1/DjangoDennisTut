7cc4fba685329e34ceec904c03b1c58a6bb5c0f6
torhaa <torjus.haakens@outlook.com>
2025-06-23 15:01:59 +0200

    passing 1st variable to templates

--- base/views.py
+++ base/views.py
@@ -3,8 +3,14 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+rooms = [
+    {'id': 1, 'name': 'Room 1 - learn python'},
+    {'id': 2, 'name': 'Room 2 - Design with me'},
+    {'id': 3, 'name': 'Room 3 - Frontend developers'}
+]
+
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', {'rooms': rooms})
 
 def room(request):
     return render(request, 'room.html')
