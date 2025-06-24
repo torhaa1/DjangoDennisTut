dc404f7f411c08325c72cbd90184cc02a46c97e7
torhaa <torjus.haakens@outlook.com>
2025-06-24 14:04:35 +0200

    fixed migration

--- base/views.py
+++ base/views.py
@@ -1,25 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Room
+
 # Create your views here.
 
-rooms = [
-    {'id': 1, 'name': 'Room 1 - learn python'},
-    {'id': 2, 'name': 'Room 2 - Design with me'},
-    {'id': 3, 'name': 'Room 3 - Frontend developers'}
-]
+# rooms = [
+#     {'id': 1, 'name': 'Room 1 - learn python'},
+#     {'id': 2, 'name': 'Room 2 - Design with me'},
+#     {'id': 3, 'name': 'Room 3 - Frontend developers'}
+# ]
 
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms': rooms}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
-            break
-
+    room = Room.objects.get(id=pk)
     context = {'room': room}
     return render(request, 'base/room.html', context)
 
